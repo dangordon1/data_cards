a14c1a88a9a582ab293ca28ee0f08c95521a1763
Annika Monari <annikamonari@gmail.com>
2016-03-22 14:28:41 +0000

    parked and preselection

--- output/cards.sh
+++ output/cards.sh
@@ -3,11 +3,11 @@ cd /afs/cern.ch/user/d/dgordon/CMSSW_7_1_5/src/HiggsAnalysis/CombinedLimit/data_
 for filename in *.txt; do
 	str="$( cut -d '.' -f 1 <<< "$filename")";
         str=$str"-output.txt"
-#"$filename"
 	>> output/test3.txt 
    	combine -M Asymptotic "$filename" | tee output/test3.txt
 	mv output/test3.txt output/$str
 done
+cd ..
 cd output
 
 
